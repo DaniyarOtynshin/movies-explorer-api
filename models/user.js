da3b2d561786dd27cb2964a80ca4aa6316358e51
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-18 22:27:35 +0600

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Поле "email" должно быть валидным email-адресом',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина поля "name" 2 символа'],
+    maxlength: [30, 'Максимальна длина поля "name" 30 символа'],
+  },
+});
+
+module.exports = model('user', userSchema);
