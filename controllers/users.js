ea94b1951923fd150398c1dc72712f35a4399bd6
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-19 02:22:58 +0600

    feat: add users controller

--- controllers/users.js
+++ controllers/users.js
@@ -8,18 +8,34 @@ const getCurrentProfile = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь не найден');
       }
-      return res.status(200).send(user);
+      return res.status(200).send({
+        email: user.email,
+        name: user.name,
+      });
     })
     .catch(next);
 };
 
 const updateCurrentProfile = (req, res, next) => {
-  User.findById(req.user._id)
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+    {
+      runValidators: true,
+      new: true,
+    },
+  )
+    .orFail(() => {
+      throw new NotFoundError('Пользователь не найден');
+    })
     .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Пользователь не найден');
-      }
-      return res.status(200).send(user);
+      res.status(200).send({
+        email: user.email,
+        name: user.name,
+      });
     })
     .catch(next);
 };
