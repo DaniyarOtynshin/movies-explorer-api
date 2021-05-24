07f8865a1db3027d5fc0381972c5eba9248f3c9e
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-24 23:02:58 +0600

    refactor: reorganize app.js

--- app.js
+++ app.js
@@ -11,39 +11,32 @@ const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, DB } = require('./config');
 
-const app = express();
-
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-
 const options = {
   origin: ['https://filmderdi-zerttewsi.nomoredomains.icu'],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
 };
-
-app.use(cors(options));
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 
-app.use('/', apiLimiter);
-app.use(helmet());
-
-app.use(bodyParser.json());
-
-app.use(requestLogger);
-
-app.use(routes);
-
-app.use(errorLogger);
-
-app.use(errors());
-app.use(errorHandler);
-
-app.listen(PORT);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    const app = express();
+    app.use(cors(options));
+    app.use('/', apiLimiter);
+    app.use(helmet());
+    app.use(bodyParser.json());
+    app.use(requestLogger);
+    app.use(routes);
+    app.use(errorLogger);
+    app.use(errors());
+    app.use(errorHandler);
+    app.listen(PORT);
+  });
